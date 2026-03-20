6d0becd65de8638f53ef2c3fe78255846546dd20
Celil Berkay Torun <cbtorun32@gmail.com>
2026-03-20 23:36:30 +0300

    Fixed #1 : Layout Page Tamamlandı

---
+++ YeniKitapKirtasiyeWebApp/Controllers/BaseController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using YeniKitapKirtasiyeWebApp.Models;
+
+namespace YeniKitapKirtasiyeWebApp.Controllers
+{
+    public class BaseController : Controller
+    {
+        protected YeniKitapKirtasiyeDBModel db = new YeniKitapKirtasiyeDBModel(); // Protected yazılmaz ise yandan ulaşılamaz
+    }
+}
