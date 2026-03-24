dbf42e9b55650f5fda1d6f199ef51e240520079f
Celil Berkay Torun <cbtorun32@gmail.com>
2026-03-24 20:18:27 +0300

    Detay Sayfası görüntüleme yapıldı

---
+++ YeniKitapKirtasiyeWebApp/Controllers/UserCategoryController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using YeniKitapKirtasiyeWebApp.Models;
+
+namespace YeniKitapKirtasiyeWebApp.Controllers
+{
+    public class UserCategoryController : Controller
+    {
+        YeniKitapKirtasiyeDBModel db = new YeniKitapKirtasiyeDBModel();
+        // GET: UserCategory
+        public ActionResult Find(int? kategoriID)
+        {
+            if (kategoriID != null)
+            {
+                return View(db.Products.Where(item => item.Category_ID == kategoriID && item.IsActive == true).ToList());
+            }
+            else
+            {
+                return RedirectToAction("Index", "UserHome");
+            }
+        }
+    }
+}
---
+++ YeniKitapKirtasiyeWebApp/Controllers/UserProductController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using YeniKitapKirtasiyeWebApp.Models;
+
+namespace YeniKitapKirtasiyeWebApp.Controllers
+{
+    public class UserProductController : Controller
+    {
+        YeniKitapKirtasiyeDBModel db = new YeniKitapKirtasiyeDBModel();
+        // GET: UserProduct
+        public ActionResult Find(int? id)
+        {
+            Product urun = db.Products.Find(id);
+            return View(urun);
+        }
+    }
+}
