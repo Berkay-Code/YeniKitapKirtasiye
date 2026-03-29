34e783e19941837e14ac47321de461182f252adf
Celil Berkay Torun <cbtorun32@gmail.com>
2026-03-29 17:59:20 +0300

    cookie ile işlemler tamamlandı

--- YeniKitapKirtasiyeWebApp/Controllers/UrunController.cs
+++ YeniKitapKirtasiyeWebApp/Controllers/UrunController.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using YeniKitapKirtasiyeWebApp.Data.ViewModel;
@@ -18,23 +20,69 @@ namespace YeniKitapKirtasiyeWebApp.Controllers
         }
         public ActionResult CookieSepeteEkle(int id)
         {
-            Product urun = db.Products.Find(id);
-            if (urun == null)
+            Product p = db.Products.Find(id);
+            if (p == null)
             {
-                return RedirectToAction("UrunNotFound");
+                RedirectToAction("UrunNotFound");
             }
-            List<SepetItem> items = new List<SepetItem>();
 
             if (Request.Cookies["Sepet"] != null)
             {
-
+                HttpCookie kurabiye = Request.Cookies["Sepet"];
+                List<SepetItem> items = JsonConvert.DeserializeObject<List<SepetItem>>(kurabiye.Value);
+                var sayi = items.Where(x => x.UrunId == id).Count();
+                if (sayi > 0)
+                {
+                    items.First(x => x.UrunId == id).Adet++;
+                    // sepete'de yönlendirme yapılabilir
+                }
+                else
+                {
+                    items.Add(new SepetItem() { UrunId = p.ID, UrunAdi = p.Name, Fiyat = p.Price, Adet = 1 });
+                }
+                var settings = new JsonSerializerSettings { StringEscapeHandling = StringEscapeHandling.EscapeNonAscii };
+                string jsoncart = JsonConvert.SerializeObject(items, Formatting.None, settings);
+                kurabiye.Value = jsoncart;
+                kurabiye.Expires = DateTime.Now.AddDays(30);
+                Response.Cookies.Add(kurabiye); //Response == Browser(Hangi browser kullanıyok ise)
             }
             else
             {
-                HttpCookie kurabiye = new HttpCookie("Sepet");
+                HttpCookie kurabiye = new HttpCookie("sepet");
+                List<SepetItem> items = new List<SepetItem>();
+                items.Add(new SepetItem() { UrunId = p.ID, UrunAdi = p.Name, Fiyat = p.Price, Adet = 1 });
+                var settings = new JsonSerializerSettings { StringEscapeHandling = StringEscapeHandling.EscapeNonAscii };
+                string jsoncart = JsonConvert.SerializeObject(items, Formatting.None, settings);
+                kurabiye.Value = jsoncart;
+                kurabiye.Expires = DateTime.Now.AddDays(30);
+                Response.Cookies.Add(kurabiye); //Response == Browser(Hangi browser kullanıyok ise)
             }
+            return RedirectToAction("index", "Urun"); // sepete'de yönlendirme yapılabilir
+        }
 
-            return View();
+        public ActionResult CookieSepetTemizle()
+        {
+            if (Request.Cookies["sepet"] != null)
+            {
+                Response.Cookies.Remove("sepet");
+                HttpCookie kurabiye = new HttpCookie("sepet");
+                kurabiye.Value = null;
+                kurabiye.Expires = DateTime.Now.AddDays(-1);
+                Response.Cookies.Add(kurabiye);
+            }
+            return RedirectToAction("Sepet");
+        }
+
+        public ActionResult CookieSepet()
+        {
+            List<SepetItem> item = new List<SepetItem>();
+
+            if (Request.Cookies["Sepet"] != null)
+            {
+                HttpCookie kurabiye = Request.Cookies["Sepet"];
+                item = JsonConvert.DeserializeObject<List<SepetItem>>(kurabiye.Value);
+            }
+            return View(item);
         }
     }
 }
